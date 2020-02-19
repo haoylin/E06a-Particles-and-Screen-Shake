dca8d0a22f4023e89bc4cb9518e6404bb87f5bc5
haoyu lin <59937983+haoylin@users.noreply.github.com>
2020-02-19 15:15:04 -0500

    finish assignment

--- Scripts/World.gd
+++ Scripts/World.gd
@@ -4,6 +4,12 @@ onready var Camera = $Camera
 
 func _ready():
 	pass
-
+	
+	
 func _physics_process(delta):
-	pass
+	if Input.is_action_pressed("fire"):
+		$Camera.add_trauma(0.1)
+	if Input.is_action_pressed("fog"):
+		$Camera.add_trauma(0.1)
+	if Input.is_action_pressed("cloud"):
+		$Camera.add_trauma(0.1)
